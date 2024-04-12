167fc5091ab52f36494bd56d2ed1c17503b867b6
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-04-12 16:33:41 +0200

    updated js by putting in try,await and async

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 // Fetching background & Author name
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-.then(response => response.json())
-.then(data => {
-    //displaying background images
-    document.body.style.backgroundImage = `url(${data.urls.full})`
-    //display name of author 
-    document.getElementById('author').textContent = `By: ${data.user.name}`
-})
-// dafault backgroundImage /author 
-.catch(error => {
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
-    document.getElementById('author').textContent = 'By: Doni Achmad';
-});
+
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await res.json()
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+} catch (err) {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+    document.getElementById("author").textContent = `By: Dodi Achmad`
+}
 
 //Fetching crypto API
-fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
-.then(response => {
-    if(!response.ok) {
+
+try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    if (!res.ok) {
         throw Error("Something went wrong")
     }
-    return response.json();
-})
-.then(data => {
-    document.getElementById('crypto-top').innerHTML = `
-    <img src=${data.image.small} />
-    <span>${data.name}</span>`
-    
-    document.getElementById('crypto').innerHTML += `
-    <p>🎯: $${data.market_data.current_price.usd}</p>
-    <p>👆: $${data.market_data.high_24h.usd}</p>
-    <p>👇: $${data.market_data.low_24h.usd}</p>`
-    
-})
-.catch(error  => console.log(error))
+    const data = await res.json()
+    document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+    `
+    document.getElementById("crypto").innerHTML += `
+        <p>🎯: $${data.market_data.current_price.usd}</p>
+        <p>👆: $${data.market_data.high_24h.usd}</p>
+        <p>👇: $${data.market_data.low_24h.usd}</p>
+    `
+} catch (err) {
+    console.error(err)
+}
 
 //learning using arrow func ,to display time and update it in a sec
 // const updateTime = () => {
@@ -53,21 +51,20 @@ function getCurrentTime() {
 setInterval(getCurrentTime, 1000)
 
 //Weather Display
-navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-    .then(response => {
-        if(!response.ok) {
-            throw Error('Weather data is not available')
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+        const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        if (!res.ok) {
+            throw Error("Weather data not available")
         }
-        return response.json();
-    })
-    .then(data => {
-        // getting the weather icon
-        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-        document.getElementById('weather').innerHTML = `
-        <img src=${iconUrl} />
-        <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-        <p class="weather-city">${data.name}</p>`
-    })
-    .catch(error => console.log(error));
-})
+        const data = await res.json()
+        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        document.getElementById("weather").innerHTML = `
+            <img src=${iconUrl} />
+            <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+            <p class="weather-city">${data.name}</p>
+        `
+    } catch (err) {
+        console.error(err)
+    }
+});
